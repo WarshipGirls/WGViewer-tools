e4a5422250895bf352f0edabfda3c7c75cc6bffa
pwyq <y433wu@edu.uwaterloo.ca>
2020-12-10 21:02:43 -0500

    Update process_fiddler_response.py

--- general/process_fiddler_response.py
+++ general/process_fiddler_response.py
@@ -1,9 +1,12 @@
 import os
+import json
+
+import chardet
 import zlib
 from ast import literal_eval
 
-i = 0
-while i < 155:
+i = 390
+while i < 391:
     filename = f'{i}_.txt'
     if not os.path.exists(filename):
         i += 1
@@ -12,7 +15,14 @@ while i < 155:
         raw = f.read()
 
     byte_data = zlib.decompress(raw)
-    data = literal_eval(byte_data.decode('ascii'))  # encoding type determined by chardet
+    try:
+        data = literal_eval(byte_data.decode('ascii'))  # encoding type determined by chardet
+    except ValueError:
+        # Still failed for passLevel.json
+        string = str(byte_data)
+        replaced = string.replace('false','{}')
+        dataform = str(replaced).strip("'<>() ").replace('\'', '\"')
+        data = json.loads(dataform)
     print("\n")
     print(f"==================== {i} ====================")
     print(data)
